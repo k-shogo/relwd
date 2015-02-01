58418f2e4e4c81af17da8937acf4747b38f0e8d1
Shogo Kawaguchi <platycod0n.ramosa@gmail.com>
2015-02-01 12:16:44 +0900

    Redisキーを生成する

---
+++ lib/relwd/base.rb
@@ -0,0 +1,35 @@
+module Relwd
+  class Base
+    include Helper
+
+    attr_accessor :type
+
+    def initialize type = :default
+      @type = type
+    end
+
+    def namespace
+      'relwd'
+    end
+
+    def base
+      "#{namespace}:index:#{type}"
+    end
+
+    def base_and p
+      "#{base}:#{p}"
+    end
+
+    def database
+      "#{namespace}:data:#{type}"
+    end
+
+    def cachebase
+      "#{namespace}:cache:#{type}"
+    end
+
+    def cachekey words
+      "#{cachebase}:#{words.join('|')}"
+    end
+  end
+end
