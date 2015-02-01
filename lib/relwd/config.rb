4e0be0bf4962bd686987a43a5e037bed60ad4fca
Shogo Kawaguchi <platycod0n.ramosa@gmail.com>
2015-02-01 12:12:51 +0900

    キャッシュ有効期限及びサジェスト数を設定できる

--- lib/relwd/config.rb
+++ lib/relwd/config.rb
@@ -1,16 +1,23 @@
 module Relwd
   module Config
     DEFAULT_MIN_COMPLETE = 2
+    DEFAULT_CACHE_EXPIRE = 600
+    DEFAULT_MATCH_LIMIT  = 10
 
-    attr_writer :min_complete
+    attr_writer :min_complete, :cache_expire, :match_limit
 
     def min_complete
       @min_complete ||= DEFAULT_MIN_COMPLETE
     end
 
-    # Accepts:
-    #   1. A Redis URL String 'redis://host:port/db'
-    #   2. An existing instance of Redis, Redis::Namespace, etc.
+    def cache_expire
+      @cache_expire ||= DEFAULT_CACHE_EXPIRE
+    end
+
+    def match_limit
+      @match_limit ||= DEFAULT_MATCH_LIMIT
+    end
+
     def redis=(server)
       if server.is_a?(String)
         @redis = nil
@@ -22,8 +29,6 @@ module Relwd
       redis
     end
 
-    # Returns the current Redis connection. If none has been created, will
-    # create a new one.
     def redis
       @redis ||= (
         url = URI(@redis_url || ENV["REDIS_URL"] || "redis://127.0.0.1:6379/0")
@@ -35,6 +40,11 @@ module Relwd
           :password => url.password
         })
       )
+    end
+
+    def configure
+      yield self
+      self
     end
 
   end
