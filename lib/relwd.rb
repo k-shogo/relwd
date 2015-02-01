cb991f6e9f31a20bafb38403ae04b2827bcfe309
Shogo Kawaguchi <platycod0n.ramosa@gmail.com>
2015-02-01 12:20:37 +0900

    デリゲートを定義

--- lib/relwd.rb
+++ lib/relwd.rb
@@ -1,7 +1,20 @@
 require 'redis'
+require 'multi_json'
 require "relwd/version"
 require "relwd/config"
+require "relwd/helper"
+require "relwd/base"
+require "relwd/backend"
+require "relwd/matcher"
 
 module Relwd
   extend Config
+
+  def self.backend type = :default
+    Backend.new type
+  end
+
+  def self.matcher type = :default
+    Matcher.new type
+  end
 end
