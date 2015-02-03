77314892aa8fa84082a1ec765e554cb1f6082164
Shogo Kawaguchi <platycod0n.ramosa@gmail.com>
2015-02-03 13:26:29 +0900

    モジュールトップ階層にインスタンスへのデリゲートを追加

--- lib/spinel.rb
+++ lib/spinel.rb
@@ -14,4 +14,13 @@ module Spinel
     Client.new type
   end
 
+  def self.method_missing(method_name, *args, type: :default, &block)
+    return super unless new(type).respond_to?(method_name)
+    new(type).send(method_name, *args, &block)
+  end
+
+  def self.respond_to?(method_name, include_private = false)
+    new.respond_to?(method_name, include_private) || super
+  end
+
 end
