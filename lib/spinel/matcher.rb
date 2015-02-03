dbfcf3f7e959ad07ea03664b35bea37a265a7087
Shogo Kawaguchi <platycod0n.ramosa@gmail.com>
2015-02-03 11:32:14 +0900

    余計な一時変数を削除

--- lib/spinel/matcher.rb
+++ lib/spinel/matcher.rb
@@ -5,25 +5,19 @@ module Spinel
       options = { limit: Spinel.match_limit, cache: true }.merge(options)
 
       words = squish(term).split.reject{|w| w.size < Spinel.min_complete}.sort
-
       return [] if words.empty?
 
       tmp_cachekey = cachekey(words)
 
-      if !options[:cache] || !Spinel.redis.exists(tmp_cachekey) || Spinel.redis.exists(tmp_cachekey) == 0
-        interkeys = words.map { |w| base_and w }
+      unless options[:cache] && Spinel.redis.exists(tmp_cachekey)
+        interkeys = words.map{ |w| base_and w }
         Spinel.redis.zinterstore(tmp_cachekey, interkeys)
         Spinel.redis.expire(tmp_cachekey, Spinel.cache_expire)
       end
 
       ids = Spinel.redis.zrevrange(tmp_cachekey, 0, options[:limit] - 1)
-      if ids.size > 0
-        results = Spinel.redis.hmget(database, *ids)
-        results = results.reject{ |r| r.nil? }
-        results.map { |r| MultiJson.decode(r) }
-      else
-        []
-      end
+      ids.empty? ? [] : Spinel.redis.hmget(database, *ids).compact.map{|json| MultiJson.decode(json)}
+
     end
 
   end
